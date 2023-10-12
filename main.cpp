11b5b2693ae4eb288f6ac392ea8c51b3a55e7cce
Ryan Bissett <pipding@gmail.com>
2023-10-12 22:38:45 +0100

    Add a ducky and render him to screen

--- main.cpp
+++ main.cpp
@@ -30,10 +30,26 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    // Camera & model loading borrowed from https://www.youtube.com/watch?v=TTa75ocharg
+    // TODO: Remove the ducky (both code and assets)
+    Model model = LoadModel("assets/models/RubberDuck_LOD0.obj"); // This model & texture come from https://www.cgtrader.com/items/2033848/download-page
+    Texture2D tex = LoadTexture("assets/textures/RubberDuck_AlbedoTransparency.png");
+    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = tex;
+
+    Camera cam = {0};
+    cam.position = (Vector3){50.0f, 50.0f, 50.0f};
+    cam.target = (Vector3){0.0f, 0.0f, 0.0f};
+    cam.up = (Vector3){0.0f, 1.0f, 0.0f};
+    cam.fovy = 90.f;
+    cam.projection = CAMERA_PERSPECTIVE;
+
+    Vector3 pos = {0.f, 0.f, 0.f};
+    BoundingBox bounds = GetMeshBoundingBox(model.meshes[0]);
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -46,17 +62,23 @@ int main(void)
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
 
+        UpdateCamera(&cam, CAMERA_THIRD_PERSON);
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(RAYWHITE);
+        BeginMode3D(cam);
+        DrawModel(model, pos, 1.f, WHITE);
+        DrawGrid(20, 20.f);
+        DrawBoundingBox(bounds, GREEN);
+        EndMode3D();
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
+
+    UnloadModel(model);
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
