71f1f92bd45659df4a83ea7af1b95d6e2d9cd181
Pipding <pipding@gmail.com>
2023-11-01 10:24:33 +0000

    Name function args in TrainCar.h

--- src/classes/TrainCar.h
+++ src/classes/TrainCar.h
@@ -9,7 +9,7 @@ public:
     TrainComponent* engine;
 
     TrainCar();
-    TrainCar(Model, Texture2D, TrainComponent*, Vector3 = {0.f, 0.f, 0.f}, Vector3 = {0.f, 0.f, 0.f});
+    TrainCar(Model model, Texture2D texture, TrainComponent* engine, Vector3 position = {0.f, 0.f, 0.f}, Vector3 rotation = {0.f, 0.f, 0.f});
 
     void update();
 };
