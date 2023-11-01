329f0f6aa55c710bbe535187d2f4d49815512a3e
Pipding <pipding@gmail.com>
2023-11-01 00:21:26 +0000

    Add a new Hostile class which just tries to get within a certain radius of a a given Vector

--- main.cpp
+++ main.cpp
@@ -10,6 +10,42 @@ bool g_paused = false;
 
 static SpaceTrainDebug& _debug = SpaceTrainDebug::getInstance();
 
+class Hostile : public Actor {
+public:
+    Vector3* target;
+    float speed;
+    float targetDistance;
+
+    Hostile(Vector3 position, Model model, Texture texture, Vector3* target)
+    :Actor(position, model, texture) {
+        this->speed = 5.f;
+        this->target = target;
+        this->targetDistance = 200.f;
+    }
+
+    void update() {
+        // Move self towards target
+
+        // TODO: This code is stolen from the TrainCar
+        // Figure out the rotation of this thing
+        Vector3 vectorBetweenMeAndTarget = Vector3Subtract(*this->target, this->position);
+        Vector3 pulledDirection = Vector3Normalize(vectorBetweenMeAndTarget);
+
+        // Don't get too close to the target
+        if (Vector3Length(vectorBetweenMeAndTarget) >= targetDistance) {
+            this->position = Vector3Add(this->position, Vector3Scale(pulledDirection, this->speed));
+        }
+
+        // Angle (in rads) between 2 vectors is given by atan2
+        float angleBetweenDucks = atan2(pulledDirection.x, pulledDirection.z);
+        // For whatever reason the angle is offset by 90 degrees so we need to subtract that. 1.5708rad = 90deg
+        // TODO: Probably should figure out why the angle is offset from what you expected
+        this->setRotation({0, angleBetweenDucks- 1.5708f, 0});
+
+        Actor::update();
+    }
+};
+
 int main(void)
 {
     //==================================================
@@ -34,6 +70,9 @@ int main(void)
     // This borrowed from the models_box_collisions example: https://github.com/raysan5/raylib/blob/master/examples/models/models_box_collisions.c
     Vector3 upgradeTowerPos = { 120.0f, 0.f, 120.f };
     Vector3 upgradeTowerSize = { 50.0f, 100.0f, 50.0f };
+
+    // WIP: Add an enemy
+    Hostile hostile({1000.f, 0.f, 1000.f}, duckModel, duckTexture, &engine.position);
 
     bool collision = false;
 
@@ -113,6 +152,7 @@ int main(void)
             carriage.update();
             carriage2.update();
             targetCam.update();
+            hostile.update();
         }
 
         // Draw
@@ -125,6 +165,7 @@ int main(void)
         engine.draw();
         carriage.draw();
         carriage2.draw();
+        hostile.draw();
         DrawGrid(2000, 20.f);
         EndMode3D();
 
