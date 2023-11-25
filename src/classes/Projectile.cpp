29bd3b66261de6e8a403738d38839add6ea0892c
Pipding <pipding@gmail.com>
2023-11-25 13:56:42 +0000

    Implement isAlive in Projectile class

--- src/classes/Projectile.cpp
+++ src/classes/Projectile.cpp
@@ -3,6 +3,7 @@
 Projectile::Projectile(Vector3 position, float speed, Vector3* destination, Model* model, Texture2D* texture)
     :Actor(position, *model, *texture), destination(destination) {
         this->speed = speed;
+        this->alive = true;
 
         // TODO: This should really be a parameter but in the interest of time it's hard-coded for now
         this->scale = 8.f;
@@ -11,4 +12,8 @@ Projectile::Projectile(Vector3 position, float speed, Vector3* destination, Mode
 void Projectile::update(float deltaTime) {
 
     Actor::update();
+}
+
+bool Projectile::isAlive() {
+    return this->alive;
 }
