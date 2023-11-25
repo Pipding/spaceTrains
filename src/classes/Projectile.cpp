fb62c1d72055c8d6b5e908d90245ef1d5819c830
Pipding <pipding@gmail.com>
2023-11-25 19:20:14 +0000

    Add a damage field to Projectiles

--- src/classes/Projectile.cpp
+++ src/classes/Projectile.cpp
@@ -1,8 +1,9 @@
 #include "Projectile.h"
 
-Projectile::Projectile(Vector3 position, float speed, Vector3* destination, Model model, Texture2D texture)
+Projectile::Projectile(Vector3 position, float speed, int damage, Vector3* destination, Model model, Texture2D texture)
     :Actor(position, model, texture), destination(destination) {
         this->speed = speed;
+        this->damage = damage;
         this->alive = true;
 
         // TODO: This should really be a parameter but in the interest of time it's hard-coded for now
@@ -28,4 +29,8 @@ void Projectile::update(float deltaTime) {
 
 bool Projectile::isAlive() {
     return this->alive;
+}
+
+int Projectile::getDamage() {
+    return this->damage;
 }
