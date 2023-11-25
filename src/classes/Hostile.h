886e552819af45e5db9b1c7a7bb7df0a45f4ec4a
Pipding <pipding@gmail.com>
2023-11-25 16:39:35 +0000

    Add an outline of the required functions to Hostile.h

--- src/classes/Hostile.h
+++ src/classes/Hostile.h
@@ -4,11 +4,34 @@
 #include "src/interfaces/ICombatant.h"
 #include "src/interfaces/IUpdatable.h"
 
+// TODO:
+// What does a hostile want to do?
+// Get within weapons range of the player
+// If they're within shooting distance, try to orbit the player
+// Shoot at the player
+// If their health gets low, run away
+
+
+
+
 class Hostile : public Actor, public ICombatant, public IUpdatable {
+private:
+    // Hostile needs to get within this range before it can fire its weapon
+    float maxEngagementDistance;
+
+    // Hostile can't fire weapons if it's too close to target.
+    // If hostile is too close to target, it should try to get away
+    float minEngagementDistance;
+
+    // How far away the Hostile is from its target
+    float distanceToTarget;
+
+    // Is the hostile trying to escape?
+    bool isFleeing = true;
+
 public:
     Vector3* target;
     float speed;
-    float targetDistance;
 
     Hostile(Vector3 position, Model model, Texture texture, Vector3* target);
 
@@ -17,6 +40,12 @@ public:
      * @param deltaTime Time in seconds for last frame drawn
     */
     void update(float deltaTime);
+
+
+
+    // ==================================================
+    // Combat-related functions
+    // ==================================================
 
     /**
      * Removes the given damageReceived from the Hostile's currentHitpoints. Will not reduce currentHitpoints below 0
