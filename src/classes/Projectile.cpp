c4ec6f18ae3a1b6d66d38dc649a443ea743e02fd
Pipding <pipding@gmail.com>
2023-11-25 13:25:01 +0000

    Replce velocity vector of proejctiles with speed float

--- src/classes/Projectile.cpp
+++ src/classes/Projectile.cpp
@@ -1,7 +1,12 @@
 #include "Projectile.h"
 
-Projectile::Projectile(Vector3 position, Vector3 velocity, Vector3* destination, Model model, Texture2D texture)
-    :Actor(position, model, texture), velocity(velocity), destination(destination) {}
+Projectile::Projectile(Vector3 position, float speed, Vector3* destination, Model model, Texture2D texture)
+    :Actor(position, model, texture), destination(destination) {
+        this->speed = speed;
+
+        // TODO: This should really be a parameter but in the interest of time it's hard-coded for now
+        this->scale = 8.f;
+    }
 
 void Projectile::update(float deltaTime) {
 
--- src/classes/Projectile.h
+++ src/classes/Projectile.h
@@ -7,7 +7,7 @@
 #include "src/interfaces/IUpdatable.h"
 class Projectile: public Actor, public IUpdatable {
 private:
-    Vector3 velocity;
+    float speed;
     Vector3* destination;
 
 public:
@@ -15,12 +15,12 @@ public:
     /**
      * Constructor
      * @param position      Initial position of the projectile
-     * @param velocity      Velocity at which the projectile will approach its target
+     * @param speed         Speed at which the projectile will approach its target
      * @param destination   Where the projectile should end up
      * @param model         The 3D model which should represent the projectile
      * @param texture       The texture to be applied to the model
     */
-    Projectile(Vector3 position, Vector3 velocity, Vector3* destination, Model model, Texture2D texture);
+    Projectile(Vector3 position, float speed, Vector3* destination, Model model, Texture2D texture);
 
     /**
      * Updates the projectile, moving it closer to its destination
