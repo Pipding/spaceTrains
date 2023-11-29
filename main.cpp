1d2a59111e9a614a9c973e975520b7e245555323
Pipding <pipding@gmail.com>
2023-11-29 12:27:12 +0000

    Add a bullet object and update the second train car to fire them

--- main.cpp
+++ main.cpp
@@ -39,11 +39,13 @@ int main(void)
     _assets.loadModel("assets/models/missile1.obj", "missile1");
     _assets.loadModel("assets/models/health_powerup.obj", "health_powerup");
     _assets.loadModel("assets/models/speed_powerup.obj", "speed_powerup");
+    _assets.loadModel("assets/models/bullet.obj", "bullet");
 
     _assets.loadTexture("assets/textures/ducky_albedo.png", "duck");
     _assets.loadTexture("assets/textures/missile1_albedo.png", "missile1");
     _assets.loadTexture("assets/textures/health_powerup_albedo.png", "health_powerup");
     _assets.loadTexture("assets/textures/speed_powerup_albedo.png", "speed_powerup");
+    _assets.loadTexture("assets/textures/bullet_albedo.png", "bullet");
 
     _assets.loadSound("assets/sounds/missile_fire.wav", "missile_fire");
 
@@ -73,8 +75,8 @@ int main(void)
         &carriage1, {-100.f, 0.f, 0.f},
         5,
         200,
-        _assets.getModel("missile1"),
-        _assets.getTexture("missile1")
+        _assets.getModel("bullet"),
+        _assets.getTexture("bullet")
     );
 
     Train train({&engine, &carriage1, &carriage2}, 100);
