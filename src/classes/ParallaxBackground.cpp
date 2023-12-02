51e3145fe1c5270e8da6073987eea7fab46af7bf
Pipding <pipding@gmail.com>
2023-12-02 13:50:13 +0000

    Add a constructor for ParallaxBackground

--- src/classes/ParallaxBackground.cpp
+++ src/classes/ParallaxBackground.cpp
@@ -2,3 +2,11 @@
 
 static AssetManager& _assets = AssetManager::getInstance();
 
+ParallaxBackground::ParallaxBackground(Vector3* parent, int layers, Model* model, Texture2D* texture)
+    : layerCount(layers), parent(parent) {
+
+        for (int i = 0; i < layers; i++) {
+            this->layerModels.push_back(model);
+            this->layerTextures.push_back(texture);
+        }
+}
