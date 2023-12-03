ca9d2c6ed157072dbae0253bea9bb5e6869e9f56
Pipding <pipding@gmail.com>
2023-12-03 02:49:57 +0000

    Git rearAttachmentPoint a default position

--- src/classes/TrainComponent.cpp
+++ src/classes/TrainComponent.cpp
@@ -3,7 +3,10 @@
 TrainComponent::TrainComponent() : Actor() {}
 
 TrainComponent::TrainComponent(Vector3 position, Model model, Texture2D texture, Model projectileModel, Texture2D projectileTexture)
-    : Actor(position, model, texture), projectileModel(projectileModel), projectileTexture(projectileTexture) {}
+    : Actor(position, model, texture), projectileModel(projectileModel), projectileTexture(projectileTexture) {
+        // TODO: This is WIP
+        this->rearAttachmentPoint = this->position;
+    }
 
 Vector3* TrainComponent::getRearAttachmentPoint() {
     return &this->rearAttachmentPoint;
